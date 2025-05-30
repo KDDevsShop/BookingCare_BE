c8c0e76683c22641b57f9836cc9bc421419130b2
npnkhoa <khoa.nphucnguyen@gmail.com>
2025-05-30 13:34:49 +0700

    feat: get doctor by specialty

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -6,6 +6,7 @@ const upload = require('../configs/multerConfig');
 router.post('/', upload.single('userAvatar'), doctorController.createDoctor);
 router.get('/', doctorController.getAllDoctors);
 router.get('/:id', doctorController.getDoctorById);
+router.get('/specialty/:specialtyId', doctorController.getDoctorsBySpecialty);
 router.put('/:id', upload.single('userAvatar'), doctorController.updateDoctor);
 router.delete('/:id', doctorController.deleteDoctor);
 
